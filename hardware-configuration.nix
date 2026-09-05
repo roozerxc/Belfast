22771fd7c943cddcb454485cbda790b8d334a116
RoozerXC <45127566+roozerxc@users.noreply.github.com>
2026-09-04 22:17:09 -0400

    Improve tab spacing

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ... }: {
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  fileSystems."/" = {
-    device = "/dev/disk/by-label/NIXOS_ROOT";
-    fsType = "ext4";
-  };
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    fileSystems."/" = {
+        device = "/dev/disk/by-label/NIXOS_ROOT";
+        fsType = "ext4";
+    };
 }
