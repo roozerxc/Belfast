1691ebc1e6e87ebafeb4490f53349069fe0e38de
RoozerXC <45127566+roozerxc@users.noreply.github.com>
2026-09-04 22:25:54 -0400

    Remove steam program

--- modules/programs.nix
+++ modules/programs.nix
@@ -1,11 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
-  };
   programs.gamemode.enable = true;
   programs.zsh.enable = true;
 
