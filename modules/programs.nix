70fd6a5aa8ced9de2038cbd92f0b999139179515
RoozerXC <45127566+roozerxc@users.noreply.github.com>
2026-09-04 22:27:06 -0400

    Remove xkb settings
    US,IL does not account for QWERTY, DVORAK, QWERTZ, AZERTY layouts

--- modules/programs.nix
+++ modules/programs.nix
@@ -3,10 +3,4 @@
 {
   programs.gamemode.enable = true;
   programs.zsh.enable = true;
-
-  services.xserver.xkb = {
-    layout = "us,il";
-    variant = ",";
-    options = "grp:alt_shift_toggle";
-  };
 }
