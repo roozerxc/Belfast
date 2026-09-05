f75775f02567e169d9a96e3d2ff8ec8c7fe89ae9
RoozerXC <45127566+roozerxc@users.noreply.github.com>
2026-09-04 22:17:02 -0400

    Update valkyrie flake to Belfast

--- flake.nix
+++ flake.nix
@@ -1,20 +1,22 @@
 {
-  description = "Valkyrie Flake";
+    description = "Belfast Flake";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-    nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
-  };
-
-  outputs = { nixpkgs, nix-flatpak, ... }@inputs: {
-    nixosConfigurations = {
-      valkyrie = nixpkgs.lib.nixosSystem {
-        modules = [
-          ./configuration.nix
-          nix-flatpak.nixosModules.nix-flatpak
-        ];
-        specialArgs = { inherit inputs; };
-      };
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+        nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
     };
-  };
+
+    outputs = { nixpkgs, nix-flatpak, ... }
+
+    @inputs: {
+        nixosConfigurations = {
+            belfast = nixpkgs.lib.nixosSystem {
+                modules = [
+                    ./configuration.nix
+                    nix-flatpak.nixosModules.nix-flatpak
+                ];
+                specialArgs = { inherit inputs; };
+            };
+        };
+    };
 }
