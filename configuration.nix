ece36dbbc3e31b4b05775dd9cf2a8d4f8a2c6679
RoozerXC <45127566+roozerxc@users.noreply.github.com>
2026-09-04 22:16:42 -0400

    Slim configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,23 +1,15 @@
 { config, pkgs, inputs, ... }:
 
 {
-  imports = [
-    ./hardware-configuration.nix
-    ./modules/packages.nix
-    ./modules/amd.nix
-    ./modules/programs.nix
-    ./modules/services.nix
-    ./modules/virtualization.nix
-    ./modules/plasma.nix
-  ];
+    imports = [
+        ./hardware-configuration.nix
+        ./modules/packages.nix
+        ./modules/programs.nix
+        ./modules/services.nix
+        ./modules/virtualization.nix
+        ./modules/plasma.nix
+    ];
 
-  networking.hostName = "valkyrie";
-  networking.networkmanager.enable = true;
-  time.timeZone = "Asia/Jerusalem";
-  i18n.defaultLocale = "en_US.UTF-8";
-
-#  system.copySystemConfiguration = true;
-  system.stateVersion = "26.05";
-
-
+    networking.networkmanager.enable = true;
+    system.stateVersion = "26.05";
 }
